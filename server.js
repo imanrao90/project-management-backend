1481330134df8bc0cc998e9b731ad0c3e8344d0d
imanrao90 <imanrao90@gmail.com>
2025-11-11 10:04:05 +0500

    inngest function added to workspace

--- server.js
+++ server.js
@@ -3,8 +3,9 @@ import 'dotenv/config'
 import cors from 'cors'
 import { clerkMiddleware } from '@clerk/express'
 import { serve } from "inngest/express";
-import serverless from 'serverless-http';
 import { inngest, functions } from "./inngest/index.js"
+import workspaceRouter from './routes/workspaceRoutes.js';
+import { protect } from './middlewares/authMiddleware.js';
 
 
 const app = express()
@@ -16,6 +17,9 @@ app.use(cors())
 app.get('/', (req, res) => res.send('Server is live!'))
 
 app.use("/api/inngest", serve({ client: inngest, functions }));
+
+// Routes
+app.use('/api/workspaces', protect, workspaceRouter)
 
 const PORT = process.env.PORT || 5000
 
